59e49a1591256229bbf7874d1268a836cb476833
wangxu1988 <blood_o@126.com>
2018-03-08 03:26:04 +0800

    ver1.0

--- test_for_dfp.py
+++ test_for_dfp.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
+import pandas as pd
 import tushare as ts
 
+class KLine():
+    def __init__(self,data_value):
+        data_list = data_value.tolist()
+        self.date = data_list[0]
+        self.open = data_list[1]
+        self.close = data_list[2]
+        self.high = data_list[3]
+        self.low = data_list[4]
+        self.volume = data_list[5]
+        self.code = data_list[6]
+        
 data = ts.get_k_data('002049', start="2018-01-01", end="2018-03-07")
 
-print data
+
+print(data.values[0].tolist())
+KLine0 = KLine(data.values[0])
+print KLine0.date
+print type(KLine0.date)
+print KLine0.open
+print KLine0.close
+print KLine0.high
+print KLine0.low
+print KLine0.volume
+
+print KLine0.code
+print type(KLine0.code)
+
